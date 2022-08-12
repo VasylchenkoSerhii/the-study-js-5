eaea841720d68015a28c47e49b15c62d69ebe3c8
Vasylchenko Serhii <darnki@gmail.com>
2022-08-12 18:30:51 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,70 +1,59 @@
 import './css/styles.css';
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import { refs } from './js/refs';
+import { Notify } from 'notiflix';
 
-
-const refs = {
-    input: document.querySelector('[id="search-box"]'),
-    list: document.querySelector('.country-list'),
-    info: document.querySelector('.country-info'),
-};
 const DEBOUNCE_DELAY = 300;
 
-refs.input.addEventListener('input', onInput);
 
+refs.input.addEventListener('input', debounce(handleSearchCountries, DEBOUNCE_DELAY));
 
-
-
-function onInput(e) {
-    let nameCountry = e.currentTarget.value;
-    fetchCountry(nameCountry);
-}; 
-
-function fetchCountry(name) {
-    return fetch(`https://restcountries.com/v2/name/${name}`)
-        .then(response => response.json())
+function handleSearchCountries(e) {
+    const value = e.target.value.trim()
+    console.log(value)
+    if (!value) {
+        refs.info.innerHTML = '';
+        refs.list.innerHTML = '';
+        return;
+    }
+    fetchCountries(value)
         .then(data => {
+            if (data.length <= 10) {
+                refs.list.innerHTML = createCountriesList(data);
+                refs.info.innerHTML = '';
+            }
+            if (data.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name.');
+            }
             if (data.length === 1) {
-                refs.list.textContent = '';
-                createCountryDescription(data);
-                return;
-            } else if (data.length <= 10) {
-                refs.info.textContent = '';
-                refs.list.textContent = '';
-                createCountrysList(data);
-                return;
-            } else if (data.length > 10) {
-                alert('Too many matches found. Please enter a more specific name.');
-            } else if (data.status === 404) {
-                alert('Oops, there is no country with that name');
-            } else if(data.message === 'Page Not Found') {
-                refs.info.textContent = '';
-                refs.list.textContent = '';
+                refs.list.innerHTML = '';
+                refs.info.innerHTML = createCountryDescription(data);
             }
         })
-        .catch(error => console.log(error));
-}
+        .catch(error => Notify.failure('Oops, there is no country with that name'));
+};
 
-function createCountrysList(array) {
-    const marKup = array.reduce((acc, el) =>
-        acc + `<li class='country-list__item'><img class='country-list__img' src='${el.flag}' alt='${el.name}' width=100 height=50><p>${el.name}</p></li>`, '');
-    refs.list.insertAdjacentHTML('beforeend', marKup);
+function createCountriesList(data) {
+    return data.reduce((acc, el) =>
+        acc + `<li class='country-list__item'><img class='country-list__img' src='${el.flags.svg}' alt='${el.name.common}' width=100 height=50><h2>${el.name.common}</h2></li>`, '');
 };
 
 function createCountryDescription(data) {
-    const marKup = data.reduce((acc, el) => acc + createMarKupDiscription(el) ,'')
-    
-    refs.info.insertAdjacentHTML('beforeend', marKup);
+    return data.reduce((acc, el) => acc + createMarKupDiscription(el) ,'')
+
 };
 
-function createMarKupDiscription({ flag, name, capital, population, languages, }) {
+function createMarKupDiscription({ flags, name, capital, population, languages, }) {
+    const lang = Object.values(languages).join(', ');
     const marKup = `<div class='country-info__name'>
-                        <img class='country-list__img' src='${flag}' alt='${name}' width=50 height=25>
-                        <h2>${name}</h2>
+                        <img class='country-list__img' src='${flags.svg}' alt='${name.common}' width=50 height=25>
+                        <h2>${name.common}</h2>
                     </div>
                     <div calss='country-info__discription'>
-                    <p><b>Capital:</b> ${capital}</p>
+                    <p><b>Capital:</b> ${capital[0]}</p>
                     <p><b>Population:</b> ${population}</p>
-                    <p><b>Languages:</b> ${languages[0].name}</p>
+                    <p><b>Languages:</b> ${lang}</p>
                     </div>`;
     return marKup;
 };
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,17 @@
-fetch('https://restcountries.com/v2/name/ukraine').then(response => response.json()).then(date => console.log(date))
+import { refs } from "./refs";
+import { createCountryDescription, createCountrysList } from "../index";
+
+const BASE_URL = 'https://restcountries.com/v3.1';
+const FILTER_RESPONSE = 'name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+    return fetch(`${BASE_URL}/name/${name}?field=${FILTER_RESPONSE}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+};
+
+
